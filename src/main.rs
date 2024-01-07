04b7a540c1ad611ccf1f6670d47783d1127cdeb0
Rh4096 <bionicvnb@gmail.com>
2024-01-07 09:54:33 +0700

    Add subcommand for creating a new directory

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(subcommand)]
+    subcmd: DirConCommand,
+}
+
+#[derive(Subcommand, Debug)]
+enum DirConCommand {
+    NewDir{
+        path: Option<PathBuf>
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
